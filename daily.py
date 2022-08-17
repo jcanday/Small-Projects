e51770a0a67beed7e81a47ab6fb0151617ac1332
jcanday <jc@milkstreetmarketing.com>
2022-08-17 12:25:37 -0400

    DFS

--- daily.py
+++ daily.py
@@ -1,17 +1,60 @@
-# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
-# This is also the LeetCode question of the day here: https://leetcode.com/problems/first-unique-character-in-a-string/
-def func(s):
-    d = {}
-    for i in s:
-        if i in d:
-            d[i] += 1
-        else: 
-            d[i] = 1
+# # Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+# # This is also the LeetCode question of the day here: https://leetcode.com/problems/first-unique-character-in-a-string/
+# def func(s):
+#     d = {}
+#     for i in s:
+#         if i in d:
+#             d[i] += 1
+#         else: 
+#             d[i] = 1
             
-    for i in range(len(s)):
-        if d[s[i]] <= 1:
-            return i
+#     for i in range(len(s)):
+#         if d[s[i]] <= 1:
+#             return i
         
-    return -1 
+#     return -1 
 
-print(func("loveleetcode"))
+# print(func("loveleetcode"))
+
+
+
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+# 'a' maps to ".-",
+# 'b' maps to "-...",
+# 'c' maps to "-.-.", and so on.
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+# Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+# For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+# Return the number of different transformations among all words we have.
+# Example 1:
+# Input: words = ["gin","zen","gig","msg"]
+# Output: 2
+# Explanation: The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+# There are 2 different transformations: "--...-." and "--...--.".
+# Example 2:
+# Input: words = ["a"]
+# Output: 1
+# Constraints:
+# 1 <= words.length <= 100
+# 1 <= words[i].length <= 12
+# words[i] consists of lowercase English letters.
+
+def func(words):
+    morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    d = {}
+    trans = []
+    for i in range(len(alph)):
+        d[alph[i]] = morse[i]
+    for i in words:
+        trans.append("".join(list(map(lambda x:d[x], i))))
+        
+    return len(list(set(trans)))
+
+print(func(["gin","zen","gig","msg"]))
+    
---
+++ dfs.py
@@ -0,0 +1,70 @@
+from collections import deque
+from this import d
+
+
+class Node:
+    def __init__(self,value,left=None,right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+    
+    def __str__(self) -> str:
+        return "Node(" + str(self.value) + ")"
+    
+#recursive dfs
+#preorder
+def walk(tree):
+    if tree is not None:
+        print(tree)
+        walk(tree.left)
+        walk(tree.right)
+        
+#inorder
+def walk2(tree):
+    if tree is not None:
+        walk2(tree.left)
+        print(tree)
+        walk2(tree.right)
+        
+#postorder
+def walk3(tree):
+    if tree is not None:
+        walk3(tree.left)
+        walk3(tree.right)
+        print(tree)
+        
+#iterative
+def walk4(tree,stack=[]):
+    stack.append(tree)
+    while len(stack) > 0:
+        node = stack.pop()
+        if node is not None:
+            print(node)
+            stack.append(node.right)
+            stack.append(node.left)
+            
+mytree = Node('A', Node('B',Node('D'),Node('E')),Node('C',Node('F'),Node('G')))
+
+
+walk3(mytree)
+
+#Binary Tree Depth
+#recursive DFS
+def maxDepth(self, root: Node) -> int:
+    if not root:
+        return 0
+    return 1+ max(maxDepth(root.left),maxDepth(root.right))
+
+#iterative BFS
+def maxDepth1(self, root: Node) -> int:
+    if not root:
+        return 0
+    
+    level = 1
+    q = deque([root])
+    while q:
+        for i in range(len(q)):
+            q.popleft()
+            i
+        level += 1
+
