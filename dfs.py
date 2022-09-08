3412dae921c06c4ba76eba925963af3d8fe97d8f
jcanday <jc@milkstreetmarketing.com>
2022-09-08 16:08:35 -0400

    binary tree string

--- dfs.py
+++ dfs.py
@@ -86,3 +86,24 @@ def maxDepth2(self, root: Node) -> int:
             stack.append([node.right],depth+1)
     
     return res
+
+# Given the root of a binary tree, construct a string consisting of parenthesis and integers from a binary tree with the preorder traversal way, and return it.
+
+# Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [1,2,3,4]
+# Output: "1(2(4))(3)"
+# Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+def tree2str(root):
+    if root == None:
+        return ""
+    if root.left == None and root.right == None:
+        return str(root.val)
+    if(root.right == None):
+        return f"{root.val}({self.tree2str(root.left)})"
+    return f"{root.val}({self.tree2str(root.left)})({self.tree2str(root.right)})"
