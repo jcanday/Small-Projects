9b60abe55a9f6bd33a4ef72d907400cccaa593d2
jcanday <jc@milkstreetmarketing.com>
2022-09-09 18:24:52 -0400

    c#

---
+++ C#/Exercise Files/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there" + str);
+
+
