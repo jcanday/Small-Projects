3aa5d7a24ffce6bae863f8c4543a96f5d36d7488
jcanday <jc@milkstreetmarketing.com>
2022-09-16 11:19:19 -0400

    C# Functions

--- C#/Exercise Files/Start/Functions/MultiValues/Program.cs
+++ C#/Exercise Files/Start/Functions/MultiValues/Program.cs
@@ -7,16 +7,28 @@ namespace MultiValues
         static void Main(string[] args)
         {
             // TODO: Tuples are grouped values added in C# 7
-
-
+            
             // TODO: Tuple values are mutable
+            (int a, int b) tup1 = (5,10);
+            var tup2 = ("Some Text", 10.5f);
 
+            tup1.b = 20;
+            tup2.Item1 = "New String";
+            Console.WriteLine($"{tup1.a},{tup1.b}");
+            Console.WriteLine($"{tup2.Item1},{tup2.Item2}");
 
             // TODO: Functions can work with tuples
+            (int, int) result = PlusTimes(6,12);
+            Console.WriteLine($"Sum: {result.Item1}, Product: {result.Item2}");
+
+           
 
         }
 
         // TODO: Functions can return multiple values using tuples
+         static(int, int) PlusTimes(int a, int b){
+                return(a+b, a*b);
+            }
 
     }
 }
--- C#/Exercise Files/Start/Functions/RefAndOutParams/Program.cs
+++ C#/Exercise Files/Start/Functions/RefAndOutParams/Program.cs
@@ -4,8 +4,8 @@ namespace RefAndOutParams {
     class Program {
         // TODO: Ordinary value arguments cannot be modified by the function
         // because they are a copy of the original value
-        static void TestFunc1(int arg1) {
-
+        static void TestFunc1(ref int arg1) {
+            arg1 += 10;
             Console.WriteLine($"{arg1}");
         }
 
@@ -15,23 +15,29 @@ namespace RefAndOutParams {
 
         // TODO: The "out" keyword means that the parameter returns a value and is not
         // used to supply data to the function
-
+        static void PlusTimes(int arg1, int arg2, out int sum, out int product){
+            sum = arg1 + arg2;
+            product = arg1 * arg2;
+        }
 
         static void Main(string[] args) {
             int val1 = 10;
             int val2 = 20;
 
             // Functions don't normally modify value arguments
-            TestFunc1(val1);
-            Console.WriteLine($"{val1}");
+            // TestFunc1(val1);
+            // Console.WriteLine($"{val1}");
 
             // TODO: Using the "ref" keyword, arguments can be passed by reference
             // which allows the function to modify them
-
+            TestFunc1(ref val1);
+            Console.WriteLine($"{val1}");
 
             // TODO: The "out" keyword can be used to indicate that an argument
             // is intended to return a value and is not an input
-
+            int a, b;
+            PlusTimes(val1, val2, out a, out b);
+            Console.WriteLine($"{a}, {b}");
         }
     }
 }
